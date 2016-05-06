a83e7e1d06fb8e3b9c966b95d86d012176d0a2d5
Ryan M. Poe <rmp23@duke.edu>
2016-05-06 01:45:38 +0000

    Removing WordPress dumb emoji script... who decided it was OK to include this by default?

--- functions.php
+++ functions.php
@@ -123,6 +123,14 @@ function ourdurhamwedding_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+	
+	/* Remove the dipshit emoji javascript. The most unprofessional junk
+	 * I've ever seen in an otherwise decent CMS. Just crap.
+	 */
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
 }
 add_action( 'wp_enqueue_scripts', 'ourdurhamwedding_scripts' );
 
